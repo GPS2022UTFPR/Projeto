80a7633b191a1225a02c3d86585e48ddbb2caf20
Felipe-Sato <felipe.sato270@gmail.com>
2022-07-02 13:11:20 -0300

    Inserido script para o botao visitante

---
+++ app/login/login.js
@@ -0,0 +1,6 @@
+var btn_login = document.querySelector('.btn-login'),
+    btn_visitor = document.querySelector('.btn-visitor');
+
+    btn_visitor.addEventListener('click', function() {
+        window.location.href = "../home/index.html";
+    });
